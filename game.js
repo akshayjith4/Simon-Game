5d6e397fa5f3a39e09c8acced8a319c59d799813
akshayjith <137667911+akshayjith4@users.noreply.github.com>
2024-09-17 11:02:06 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,69 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+var soundMap = {
+    "red": new Audio('./sounds/red.mp3'),
+    "blue": new Audio('./sounds/blue.mp3'),
+    "green": new Audio('./sounds/green.mp3'),
+    "yellow": new Audio('./sounds/yellow.mp3'),
+    "wrong": new Audio('./sounds/wrong.mp3')
+};
+
+function nextSequence() {
+    userClickedPattern = [];
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    playAudio(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    level++;
+    $("#level-display").text("Level " + level);
+}
+
+function playAudio(chosenColour) {
+    if (soundMap[chosenColour]) {
+        soundMap[chosenColour].play();
+    }
+}
+
+$(".btn").click(function() {
+    if (!started) return;
+    var clickedButton = $(this);
+    var userChosenColour = clickedButton.attr('id');
+    playAudio(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1500);
+        }
+    } else {
+        //game over
+        playAudio("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("#level-display").text("Level 0");
+        level = 0;
+        gamePattern = [];
+        started = false;
+    }
+}
+
+$(document).keypress(function() {
+    if (!started) {
+        nextSequence();
+        started = true;
+        $("#level-title").text("Simon Game");
+    }
+});
